f7d7d6a14ae5827271b4f73790ecf718159860e6
Jeliel <jelielmatheus10@gmail.com>
2021-05-14 16:51:24 -0300

    Adicionando metodo de subtracao

---
+++ src/br/com/fafic/engSoftwareII/main/Subtracao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fafic.engSoftwareII.main;
+
+/**
+ *
+ * @author TECNOEASY
+ */
+public class Subtracao {
+    
+    public int subtrair(int a, int b){
+        return a + b;
+    }
+}
